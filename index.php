6941417a324ffdac9148e2f6f2bf4f94cd78f666
inzemam7 <syedinzu50@gmail.com>
2024-06-13 20:32:37 +0530

    new start

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+   <style>
+    html{
+        background-color: #E9F1F7;
+    }
+    ul li {
+        display: inline;
+        padding: 2rem;
+        text-decoration: none;
+           
+    }
+
+    .navbar{
+        background-color: #2274A5;
+        padding: 1rem;
+        border-radius: 2rem;
+    }
+
+    .button{
+        text-align: center;
+     padding: 1rem; 
+     border-radius: 2rem ; 
+     background-color: #816C61;
+    }
+    
+    .footer{
+        background-color: #2274A5;
+        padding: 1rem;
+        border-radius: 2rem;
+
+    }
+
+    .centre{
+        align-items: center;
+        width: auto;
+        padding: 1rem;
+    }
+    .image{
+        padding-top: 0.5rem;
+        border-radius: 2rem;
+    }
+
+    a{
+        padding:1rem;
+        border-radius:2rem;
+    }
+
+    a:hover {
+        background-color: #80c3f7;
+    }
+   </style>
+</head>
+
+<body>
+<header class="navbar">
+    
+        <h1>[insert hotel name]</h1>
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="adminlogin.php">ADMIN</a></li>
+            <li><a href="userlogin.php">USER</a></li>
+            <li><a href="bookings.php">BOOKINGS</a></li>
+            <li><a href="#about">ABOUT</a></li>
+        </ul>
+        </header>
+    
+        <img class="image" src="images/hm 11.webp" alt="" width="100%" height="700px">
+    <div class="centre button">
+        <a href="#">Back to top</a>
+    </div>
+    <footer  class="footer" id="about">
+        
+
+    </footer>
+</body>
+
+</html>
