e20726cea07521b3348904e9ebee2ccaf500541e
AsH-nk <nknawaz0001@gamil.com>
2024-06-21 19:51:06 +0530

    commit

--- navv/nav.php
+++ navv/nav.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
     <style>
+
       .nav-item a{
 
         position:relative;
@@ -14,8 +15,9 @@
       }
 
       .navbar {
-            background-color: #2274C9;
-            color:black;
+        position: fixed;
+        background-color: #2274C9;
+        color:black;
     
 
         }
